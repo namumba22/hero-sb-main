94ed9f3c264b90aaed3ba71a68d7d2875124e843
Andriy Dumchykov <Andriy_Dumchykov@epam.com>
2017-06-30 16:58:25 +0300

    init

--- src/main/java/com/my/EurekaApp.java
+++ src/main/java/com/my/EurekaApp.java
@@ -20,7 +20,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @SpringBootApplication
 @EnableEurekaServer
-@EnableCircuitBreaker
 @PropertySource("classpath:eureka-server.properties")
 public class EurekaApp {
 
