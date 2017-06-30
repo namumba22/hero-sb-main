ee6f4d6a422d65fbee53d8232ee69404575e69f2
Andriy Dumchykov <Andriy_Dumchykov@epam.com>
2017-06-30 12:52:08 +0300

    init

---
+++ src/main/java/com/my/EurekaApp.java
@@ -0,0 +1,34 @@
+package com.my;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by dumin on 6/29/17.
+ */
+
+@Configuration
+@EnableAutoConfiguration
+@RestController
+@SpringBootApplication
+@EnableEurekaServer
+@PropertySource("classpath:eureka-server.properties")
+public class EurekaApp {
+
+    @RequestMapping(value ="/hallow", method = RequestMethod.GET)
+    public String getStatus() {
+        return "Hallow eureka";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApp.class, args);
+    }
+
+}
