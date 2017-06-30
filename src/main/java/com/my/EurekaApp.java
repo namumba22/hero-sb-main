706d6c90d406452ff97d3041814412e7d77a7eae
Andriy Dumchykov <Andriy_Dumchykov@epam.com>
2017-06-30 16:39:49 +0300

    init

--- src/main/java/com/my/EurekaApp.java
+++ src/main/java/com/my/EurekaApp.java
@@ -3,6 +3,7 @@ package com.my;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
@@ -19,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @SpringBootApplication
 @EnableEurekaServer
+@EnableCircuitBreaker
 @PropertySource("classpath:eureka-server.properties")
 public class EurekaApp {
 
